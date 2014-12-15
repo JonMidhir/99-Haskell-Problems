a53d055b3f452530c8368f2153fa7fda3598752a
John Hope <info@midhirrecords.com>
2014-12-15 01:50:44 +0000

    Solution to problem 31

---
+++ problem31.hs
@@ -0,0 +1,5 @@
+-- Determine whether a given integer number is prime.
+--   > isPrime 7
+--   True
+isPrime :: Int -> Bool
+isPrime n = all (\x -> n `mod` x /= 0) [2..ceiling . sqrt $ fromIntegral n]

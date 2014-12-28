2788886b824cc53387cbad4626566a742825680b
John Hope <info@midhirrecords.com>
2014-12-28 14:53:32 +0000

    Solution to problem 12

---
+++ problem12.hs
@@ -0,0 +1,14 @@
+-- Decode a run-length encoded list.
+--   decodeModified 
+--    [Multiple 4 'a',Single 'b',Multiple 2 'c',
+--     Multiple 2 'a',Single 'd',Multiple 4 'e']
+--    > "aaaabccaadeeee"
+data ListItem a = Multiple Int a | Single a deriving (Show)
+
+unpack :: ListItem a -> [a]
+unpack (Multiple n a) = foldl (\acc x -> a:acc) [] [1..n]
+unpack (Single a) = [a]
+
+decodeModified :: [ListItem a] -> [a]
+decodeModified (x:[]) = unpack x
+decodeModified (x:xs) = unpack x ++ decodeModified xs
